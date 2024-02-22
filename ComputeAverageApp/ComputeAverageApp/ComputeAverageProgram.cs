f2e32b3819e08651a2dceed9d37b1154d95cd87f
Adrianne Lang <137845861+AdrianneVilla@users.noreply.github.com>
2024-02-23 07:46:50 +0800

    Add files via upload

---
+++ ComputeAverageApp/ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static int grades, newGrade;
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter 5 grades seperated by new line: ");
+
+            for(int i = 0; i < 5; i++) {
+                grades = Convert.ToInt32(Console.ReadLine());
+                newGrade += grades;
+            }
+            double average =(double) newGrade/5;
+            Console.WriteLine($"The average is {average} and round off to {Math.Round(average)} ");
+           
+            
+             
+        }
+    }
+}
