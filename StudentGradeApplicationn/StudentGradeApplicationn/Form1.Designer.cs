34be304f8f3478a7a306159879b37728f495c434
Adrianne Lang <137845861+AdrianneVilla@users.noreply.github.com>
2024-02-23 09:41:54 +0800

    Add files via upload

---
+++ StudentGradeApplicationn/StudentGradeApplicationn/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace StudentGradeApplicationn
+{
+    partial class frmStudentGradeProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Label();
+            this.StudentInformation = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grades = new System.Windows.Forms.Label();
+            this.english = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.math = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.Science = new System.Windows.Forms.Label();
+            this.Filipino = new System.Windows.Forms.Label();
+            this.History = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(36, 94);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(41, 13);
+            this.name.TabIndex = 0;
+            this.name.Text = "Name: ";
+            // 
+            // StudentInformation
+            // 
+            this.StudentInformation.AutoSize = true;
+            this.StudentInformation.Font = new System.Drawing.Font("Algerian", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentInformation.Location = new System.Drawing.Point(35, 50);
+            this.StudentInformation.Name = "StudentInformation";
+            this.StudentInformation.Size = new System.Drawing.Size(324, 32);
+            this.StudentInformation.TabIndex = 1;
+            this.StudentInformation.Text = "Student Information";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 208);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.English);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Student #: ";
+            // 
+            // grades
+            // 
+            this.grades.AutoSize = true;
+            this.grades.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grades.Location = new System.Drawing.Point(37, 174);
+            this.grades.Name = "grades";
+            this.grades.Size = new System.Drawing.Size(91, 24);
+            this.grades.TabIndex = 6;
+            this.grades.Text = "Grades";
+            // 
+            // english
+            // 
+            this.english.AutoSize = true;
+            this.english.Location = new System.Drawing.Point(38, 211);
+            this.english.Name = "english";
+            this.english.Size = new System.Drawing.Size(41, 13);
+            this.english.TabIndex = 7;
+            this.english.Text = "English";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(105, 238);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(67, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // math
+            // 
+            this.math.AutoSize = true;
+            this.math.Location = new System.Drawing.Point(40, 241);
+            this.math.Name = "math";
+            this.math.Size = new System.Drawing.Size(31, 13);
+            this.math.TabIndex = 9;
+            this.math.Text = "Math";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(105, 269);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(67, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(105, 304);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(67, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(105, 336);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(67, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // Science
+            // 
+            this.Science.AutoSize = true;
+            this.Science.Location = new System.Drawing.Point(38, 275);
+            this.Science.Name = "Science";
+            this.Science.Size = new System.Drawing.Size(46, 13);
+            this.Science.TabIndex = 13;
+            this.Science.Text = "Science";
+            // 
+            // Filipino
+            // 
+            this.Filipino.AutoSize = true;
+            this.Filipino.Location = new System.Drawing.Point(41, 310);
+            this.Filipino.Name = "Filipino";
+            this.Filipino.Size = new System.Drawing.Size(39, 13);
+            this.Filipino.TabIndex = 14;
+            this.Filipino.Text = "Filipino";
+            // 
+            // History
+            // 
+            this.History.AutoSize = true;
+            this.History.Location = new System.Drawing.Point(41, 342);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(39, 13);
+            this.History.TabIndex = 15;
+            this.History.Text = "History";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 53);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Generate Grades";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 35);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Grade Result";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(281, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "The general average of ";
+            this.label4.Visible = false;
+            // 
+            // frmStudentGradeProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.Filipino);
+            this.Controls.Add(this.Science);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.math);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.english);
+            this.Controls.Add(this.grades);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StudentInformation);
+            this.Controls.Add(this.name);
+            this.Name = "frmStudentGradeProgram";
+            this.Text = "frmStudentGradeProgram";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label StudentInformation;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label grades;
+        private System.Windows.Forms.Label english;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label math;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label Science;
+        private System.Windows.Forms.Label Filipino;
+        private System.Windows.Forms.Label History;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ StudentGradeApplicationn/StudentGradeApplicationn/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplicationn
+{
+    public partial class frmStudentGradeProgram : Form
+    {
+        public frmStudentGradeProgram()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double english = Convert.ToDouble(textBox3.Text);
+            double math = Convert.ToDouble(textBox4.Text);
+            double science = Convert.ToDouble(textBox5.Text);
+            double filipino = Convert.ToDouble(textBox6.Text);
+            double history = Convert.ToDouble(textBox7.Text);
+
+            double average = (double)(english + math + science + filipino + history) / 5;
+            label2.Visible = true;
+            label4.Visible = true;
+
+            string result = average >= 75.00 ? label4.Text = $"\tStudent Passed\nThe general average of {textBox1.Text} is {average}"
+                : label4.Text = $"\tStudent Failed\nThe general average of {textBox1.Text}is {average}";
+            
+        }
+
+        private void English(object sender, EventArgs e)
+        {
+           int english = Convert.ToInt32(Console.ReadLine());
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
